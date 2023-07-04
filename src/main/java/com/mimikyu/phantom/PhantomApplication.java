7263fb8e41239d33a8542418e8d1fa2dae49dd4a
hajihong <hajihong96@gamil.com>
2023-07-04 21:54:38 +0900

    initialize phantom-server

---
+++ src/main/java/com/mimikyu/phantom/PhantomApplication.java
@@ -0,0 +1,13 @@
+package com.mimikyu.phantom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhantomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhantomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mimikyu/phantom/PhantomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimikyu.phantom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhantomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
